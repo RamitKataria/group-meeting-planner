6e609e5de6e6550ae78b9bb718e4ee500d928fd7
sophie84 <sophie84@student.ubc.ca>
2022-06-11 18:49:49 -0700

    navbar v2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-// import NavBar from "./components/NavBar"
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavBar/>
-//     </div>
-//   );
-// }
-//
-// export default App;
-// import 'boxicons/css/boxicons.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/pages/Home';
-import NewMeeting from './components/pages/NewMeeting';
-import Account from './components/pages/Account';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Home from './components/pages/Home';
+// import NewMeeting from './components/pages/NewMeeting';
+// import Account from './components/pages/Account';
+import NavBar from './components/NavBar.js';
+// import AllMeetings from "./components/pages/AllMeetings.js";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path='/home' element={<Home />} />
-                    <Route path='/newMeeting' element={<NewMeeting />} />
-                    <Route path='/account' element={<Account />} />
-                    {/*<Route path='/order' element={<Blank />} />*/}
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <NavBar/>
+        // <BrowserRouter>
+        //     <Routes>
+        //         <Route path='/' element={<NavBar />}>
+        //             <Route index element={<Home />} />
+        //             <Route path='/home' element={<Home />} />
+        //             <Route path='/newMeeting' element={<NewMeeting />} />
+        //             <Route path='/allMeetings' element={<AllMeetings />} />
+        //             <Route path='/account' element={<Account />} />
+        //         </Route>
+        //     </Routes>
+        // </BrowserRouter>
     );
 }
 
