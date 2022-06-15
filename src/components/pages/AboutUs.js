034fa2a9e84142901bec292d91ef2557631d0fb9
sophie84 <sophie84@student.ubc.ca>
2022-06-15 03:21:39 -0700

    update gif alt

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -15,26 +15,26 @@ export default function AboutUs() {
 
 			<div className="section">
 				<img className="right image" src="https://media.giphy.com/media/PgKAsMXyo0vXFp6vj0/giphy.gif"
-					 alt="language-gif"/>
+					 alt="may-gif"/>
 					<h3>May Tang </h3>
 				<p className="right-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>
 			</div>
 			<div className="section">
 				<img className="left image" src="https://media.giphy.com/media/1jXaP2LfX7eQTErugm/giphy.gif"
-					 alt="piano-gif"/>
+					 alt="ramit-gif"/>
 					<h3>Ramit Kataria</h3>
 				<p className="left-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>
 			</div>
 
 			<div className="section">
 				<img className="right image" src="https://media.giphy.com/media/1wX7raPRBBmIjT4kmS/giphy.gif"
-					 alt="language-gif"/>
+					 alt="sophie-gif"/>
 				<h3>Sophie Chai</h3>
 				<p className="right-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>
 			</div>
 			<div className="section">
 				<img className="left image" src="https://media.giphy.com/media/l0MYIdIrGhCuFqj72/giphy.gif"
-					 alt="piano-gif"/>
+					 alt="tom-gif"/>
 				<h3>Tom Mo</h3>
 				<p className="left-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>			</div>
 			</div>
