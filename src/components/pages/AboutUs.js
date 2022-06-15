abb4cec5f01f1dbdf8d8400125398ebb143a1c1e
sophie84 <sophie84@student.ubc.ca>
2022-06-15 03:59:04 -0700

    add social media links

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,5 +1,7 @@
 import "../../css/aboutUs.css";
 
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+
 export default function AboutUs() {
 	return (
 		<div className="outer-div">
@@ -11,19 +13,25 @@ export default function AboutUs() {
 			</div>
 
 			<div className="team-div">
-			<h3>We are a team of four in UBC.</h3>
+			<h3>We are Honey Dip Donut, a team of four in UBC.</h3>
 
 			<div className="section">
 				<img className="right image" src="https://media.giphy.com/media/PgKAsMXyo0vXFp6vj0/giphy.gif"
 					 alt="may-gif"/>
 					<h3>May Tang </h3>
 				<p className="right-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>
+				<a href="#" className="icon"><FaLinkedin/></a>
+				<a href="#" className="icon"><FaGithub/></a>
+				<a href="#" className="icon"><FaEnvelope/></a>
 			</div>
 			<div className="section">
 				<img className="left image" src="https://media.giphy.com/media/1jXaP2LfX7eQTErugm/giphy.gif"
 					 alt="ramit-gif"/>
 					<h3>Ramit Kataria</h3>
 				<p className="left-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>
+				<a href="#" className="icon"><FaLinkedin/></a>
+				<a href="#" className="icon"><FaGithub/></a>
+				<a href="#" className="icon"><FaEnvelope/></a>
 			</div>
 
 			<div className="section">
@@ -31,12 +39,20 @@ export default function AboutUs() {
 					 alt="sophie-gif"/>
 				<h3>Sophie Chai</h3>
 				<p className="right-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>
+				<a href="#" className="icon"><FaLinkedin/></a>
+				<a href="#" className="icon"><FaGithub/></a>
+				<a href="#" className="icon"><FaEnvelope/></a>
 			</div>
 			<div className="section">
 				<img className="left image" src="https://media.giphy.com/media/l0MYIdIrGhCuFqj72/giphy.gif"
 					 alt="tom-gif"/>
 				<h3>Tom Mo</h3>
-				<p className="left-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>			</div>
+				<p className="left-description">Nulla dignissim, quam at consequat iaculis, risus felis vestibulum augue, eget sodales sapien ante in leo. Pellentesque tincidunt pellentesque augue. Pellentesque vestibulum, elit vitae mattis luctus, ligula nisi mollis enim, non viverra nibh ex eu magna. Cras eu semper neque, non commodo libero. Nunc sit amet volutpat sem, vitae tristique tortor.</p>
+				<a href="#" className="icon"><FaLinkedin/></a>
+				<a href="#" className="icon"><FaGithub/></a>
+				<a href="#" className="icon"><FaEnvelope/></a>
+			</div>
+
 			</div>
 		</div>
 	);
