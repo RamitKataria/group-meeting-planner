79ffe3c2d55623704d5a0f0bccfdf914b09740f1
Sophie Chai <chaisuefeisophie@gmail.com>
2022-06-24 18:23:16 -0700

    copy files from branch May

---
+++ client/May-files/popbox.js
@@ -0,0 +1,24 @@
+import "./popbox.css";
+import React, {useState} from "react";
+import Modal from "./Modal";
+import {Button} from "@mui/material";
+
+export default function CreatePopBox(){
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    return(
+       <div className= "popBox">
+           <Button variant="contained"  size ="large" onClick=
+               {()=>{setIsOpen(true)}}>
+             Continued as Guest
+           </Button>
+           {isOpen && <Modal onClose={setIsOpen}/>}
+       </div>
+
+    );
+
+
+
+}
