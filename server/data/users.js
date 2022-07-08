1f232f2431ac7dbb9a0e0624ddff5a4bf4a5fef6
Sophie Chai <chaisuefeisophie@gmail.com>
2022-07-07 17:13:58 -0700

    include ics & meetings in users schema

--- server/data/users.js
+++ server/data/users.js
@@ -4,6 +4,8 @@ exports.items = [
 		userId: "d515b255-0691-4778-9796-cb4f41840136",
 		name: "Ramit Kataria",
 		email: "ramitkataria@gmail.com",
+		ics: "https://calendar.google.com/calendar/ical/chaisuefeisophie%40gmail.com/public/basic.ics",
+		meetings: ["de382de9-799b-4351-95ed-1dd92c151263", "fc73754b-00be-4fa4-b02f-1efad9cffe05"],
 		auth: [
 			{
 				"Service": "internal",
@@ -19,6 +21,8 @@ exports.items = [
 		userId: "ea0f9ae2-2c9e-40eb-9bae-c40054addcf9",
 		name: "Tom Mo",
 		email: "tommo@gmail.com",
+		ics: "https://calendar.google.com/calendar/ical/chaisuefeisophie%40gmail.com/public/basic.ics",
+		meetings: [""],
 		auth: [
 			{
 				"Service": "internal",
@@ -34,6 +38,8 @@ exports.items = [
 		userId: "81409ec3-deac-41ef-9da2-7c0c1b324321",
 		name: "May Tang",
 		email: "maytang@gmail.com",
+		ics: "https://calendar.google.com/calendar/ical/chaisuefeisophie%40gmail.com/public/basic.ics",
+		meetings: [""],
 		auth: [
 			{
 				"Service": "internal",
@@ -49,6 +55,8 @@ exports.items = [
 		userId: "baebcdfb-4fc0-411d-9934-0d9ffa57ffe1",
 		name: "Sophie Chai",
 		email: "sophiechai@gmail.com",
+		ics: "https://calendar.google.com/calendar/ical/chaisuefeisophie%40gmail.com/public/basic.ics",
+		meetings: [""],
 		auth: [
 			{
 				"Service": "internal",
