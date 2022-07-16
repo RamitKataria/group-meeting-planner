2e774f039656c0c0876cf5bb3589ab13cae475a3
Sophie Chai <chaisuefeisophie@gmail.com>
2022-07-15 20:35:19 -0700

    split schemas into separate files

---
+++ server/model/meeting.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const {Schema, ObjectId} = require("mongoose");
+
+// create schema
+
+const MeetingSchema = new mongoose.Schema({
+	_id: String,
+	name: {
+		type: String,
+		required: [true, 'Why no meeting name?']
+	},
+	description: String,
+	dateTimeCreated: Date,
+	dateTimeUpdated: Date,
+	createdBy: { type: String, ref: 'User' },
+	range: [{
+		startDate: Date,
+		endDate: Date
+	}],
+	userAvailability: [{
+		_id : false ,
+		user:  { type: String, ref: 'User' },
+		availableSlots: [String]
+	}]
+});
+
+// create model
+const Meeting = mongoose.model('Meeting', MeetingSchema);
+
+module.exports = Meeting;
