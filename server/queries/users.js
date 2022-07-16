f58c38af05b9da5ae93cadc6dac24b961c30bb4e
Sophie Chai <chaisuefeisophie@gmail.com>
2022-07-15 20:34:03 -0700

    add user and meetings queries

---
+++ server/queries/users.js
@@ -0,0 +1,26 @@
+const User = require('../model/user');
+
+const usersQueries = {
+	// get one user
+	getUser: async function (filter) {
+		const users = await User.find(filter);
+		return users;
+	},
+	// delete one user
+	deleteOneUser: async function (id) {
+		const user = await User.findByIdAndDelete(id);
+		return user;
+	},
+	// update an existing user
+	updateOneUser: async function (id, obj) {
+		const user = await User.findByIdAndUpdate(id, obj);
+		return user;
+	},
+	// create a new user
+	insertOneUser: async function (user) {
+		const users = await User.insertMany(user);
+		return users;
+	}
+}
+
+module.exports = usersQueries;
