2a6106dcd2040d44dad5ae2cda60331887d1c1f2
Sophie Chai <chaisuefeisophie@gmail.com>
2022-07-25 16:25:57 -0700

    remove navbar2

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
-
 import NavBar from './components/NavBar.js';
-import NavBar2 from './components/Navbar2.js';
 import {BrowserRouter, Router} from "react-router-dom";
 
 function App() {
     return (
         <div>
             <BrowserRouter>
-                <NavBar2/>
+                <NavBar/>
             </BrowserRouter>
         </div>
-            // <NavBar/>
 
     );
 }
