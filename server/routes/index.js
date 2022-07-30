cc56fd1aabd9b5c945619b6c8836f2e4fe02d269
Ramit Kataria <ramit.kataria@gmail.com>
2022-07-30 13:37:32 -0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.send({message: 'Hello, you are at index'});
+    res.send({message: 'Hello World'});
 });
 
 module.exports = router;
