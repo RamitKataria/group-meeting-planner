feb786a6021e99441fe7285b9a670e67edf543f5
Ramit Kataria <ramit.kataria@gmail.com>
2022-08-07 16:55:21 -0700

    Increase robustness of user endpoints

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,89 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const {v4: uuidv4} = require('uuid');
 
-let usersData = require('../data/users');
-let items = usersData.items;
-const meetingsQueries = require("../queries/meetings");
-const usersQueries = require("../queries/users");
+const { validateFirebaseIdToken, deleteUserInFirebase, confirmAuthenticated} = require('../auth')
+const { User } = require("../db-models");
 
-router.get('/:userId/meetings/:meetingId', async function (req, res, next) {
-	if (!req.user) {
-		return res.status(403).send('Unauthorized');
+
+async function getUserOrInit(firebaseUID) {
+	let user = await User.findOne({firebaseUID: firebaseUID});
+	if (user) {
+		return user;
 	}
-	const user = await usersQueries.getUser({"_id": req.params.userId});
-	const meetings = user.meetings;
+	user = new User({firebaseUID: firebaseUID});
+	await user.save();
+	return user;
+}
 
-	if (meetings.includes(req.params.meetingId)) {
-		const meeting = meetingsQueries.getMeetings({"_id": req.params.meetingId});
-		return res.send(meeting);
-	} else {
-		return res.status(404).send({message: 'Meeting Not found'});
-	}
-});
-
-router.get('/:userId/meetings', async function (req, res, next) {
+router.get('/:userID/meetings', confirmAuthenticated, async function (req, res, next) {
 	try {
-		if (!req.user) {
-			return res.status(403).send('Unauthorized');
-		}
-		const user = await usersQueries.getUser({firebaseUID: req.params.userId});
-		return res.send(user[0].meetings);
+		const user = await getUserOrInit(req.params.userID);
+		return res.send(user.meetings);
 	} catch (e) {
 		console.log(e);
-		return res.status(403).send('Internal server error');
+		return res.status(404).send('Not found');
 	}
-
 });
 
-// router.get('/:userId', async function (req, res, next) {
-// 	if (!req.user) {
-// 		return res.status(403).send('Unauthorized');
-// 	}
-// 	const user = await usersQueries.getUser({"_id": req.params.userId});
-//
-// 	return res.send(user[0]);
-// });
-
-router.get('/:userId', async function (req, res, next) {
-	if (!req.user) {
-		return res.status(403).send('Unauthorized');
+router.get('/:userID', confirmAuthenticated, async function (req, res, next) {
+	try {
+		const user = await getUserOrInit(req.params.userID);
+		return res.send(user);
+	} catch (e) {
+		console.log(e);
+		return res.status(404).send('Not found');
 	}
-	const user = await usersQueries.getUser({"firebaseUID": req.params.userId});
-	return res.send(user[0]);
 });
 
-router.patch('/:userId', async function (req, res) {
-	if (!req.user) {
-		return res.status(403).send('Unauthorized');
+router.patch('/:userID', confirmAuthenticated, async function (req, res) {
+	try {
+		let user = await getUserOrInit(req.params.userID);
+		user = {...user, ...req.body, _id: user._id, firebaseUID: user.firebaseUID};
+		await user.save();
+		return res.send(user);
+	} catch (e) {
+		console.log(e);
+		return res.status(404).send('Not found');
 	}
-	const userArray = await usersQueries.getUser({"firebaseUID": req.params.userId});
-	const newUser = {...userArray, ...req.body, _id: userArray[0]._id, firebaseUID: userArray[0].firebaseUID};
-	await usersQueries.updateOneUser(userArray[0]._id, newUser);
-	const updatedUser = await usersQueries.getUser({"firebaseUID": req.params.userId});
-	return res.send(updatedUser[0]);
 });
 
-router.delete('/:userId', async function (req, res, next) {
-	if (!req.user) {
-		return res.status(403).send('Unauthorized');
+router.delete('/:userID', confirmAuthenticated, async function (req, res, next) {
+	try {
+		await deleteUserInFirebase(req.params.firebaseUID);
+		await User.deleteMany({firebaseUID: req.params.firebaseUID});
+		return res.status(200).send('Deleted');
+	} catch (e) {
+		return res.status(404).send('Not found');
 	}
-	const user = await usersQueries.deleteOneUser(req.params.userId);
-
-	return res.send({message: 'Deleted'});
-});
-
-router.post('/', async function(req, res, next){
-	if (!req.user) {
-		return res.status(403).send('Unauthorized');
-	}
-	if (req.body) {
-		const newUser = {...req.body, _id: uuidv4()};
-
-		await usersQueries.insertOneRecipe(newUser);
-		return res.send(newUser);
-	}
-	return res.status(400).send({message: 'Invalid body'});
 });
 
 module.exports = router;
