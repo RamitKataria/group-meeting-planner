d25cb07987cab0d8d7398250c1cca3f307c58c16
Tom Mo <tmo217@protonmail.com>
2022-08-09 17:01:54 -0700

    Revert "frontend service"
    This reverts commit 5e173488621ec66761362030686dfcafb96682e6.

--- client/src/redux/meetings/service.js
+++ client/src/redux/meetings/service.js
@@ -85,28 +85,10 @@ const updateAvailability = async (meetingId, userId, content) => {
   return data;
 }
 
-export const readICSAndUpdate = async(meetingId, userId) => {
-  const link = url + 'availability/ics/' + meetingId + '/' + userId;
-  const response = await fetch(link, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  });
-
-  const data = await response.json();
-  if (!response.ok) {
-    const errorMsg = data?.message;
-    throw new Error(errorMsg)
-  }
-  return data;
-}
-
 export default {
   addMeeting: addMeeting,
   getMeeting: getMeeting,
   deleteMeeting: deleteMeeting,
   updateMeeting: updateMeeting,
-  updateAvailability: updateAvailability, 
-  readICSAndUpdate: readICSAndUpdate,
+  updateAvailability: updateAvailability
 };
